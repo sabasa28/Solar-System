4763648f8dce1edffaeb63238ab7da062d5f2a6a
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-04-17 18:11:35 -0300

    Lograda lluvia de meteoritos y atmosfera visible en un planeta random

---
+++ Solar-System IDG/Assets/Metheor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metheor : MonoBehaviour
+{
+    float speed = 10000.0f;
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2,2),-10,Random.Range(-2, 2))*Time.deltaTime*speed);
+    }
+    void Update()
+    {
+        if (transform.position.y < -150.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Metheor")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Solar-System IDG/Assets/create.cs
+++ Solar-System IDG/Assets/create.cs
@@ -4,45 +4,74 @@ using UnityEngine;
 
 public class Create : MonoBehaviour
 {
-	public guidedCam1 cam;
+    public guidedCam1 cam;
 
-	public Movement1 planetPrefab;
+    public Movement1 planetPrefab;
 
-	public List<Material> planetMaterials;
+    public GameObject atmosphere;
 
-	public List<Movement1> generatedPlanets = new List<Movement1>();
+    public List<Material> planetMaterials;
 
-	public int planetAmount;
+    public List<Movement1> generatedPlanets = new List<Movement1>();
 
-	private float time = 0;
+    public GameObject metheor;
 
-	private void Awake()
-	{
-		for (int i = 0; i < planetAmount; i++)
-		{
-			GameObject go = Instantiate(planetPrefab, new Vector3(0, 0, 20.0f + i * 10.0f), Quaternion.identity).gameObject;
+    public int metheorAmount;
 
-			Movement1 p = go.GetComponent<Movement1>();
-			
-			go.GetComponent<MeshRenderer>().material = planetMaterials[Random.Range(0, planetMaterials.Count)];
+    public int planetAmount;
+    
+    int randAux;
 
-			p.radius = Random.Range(2, 6);
+    float timer = 0.0f;
+    private void Awake()
+    {
+        randAux = (int)Random.Range(0.0f, planetAmount - 1);
+        for (int i = 0; i < planetAmount; i++)
+        {
+            GameObject go = Instantiate(planetPrefab, new Vector3(0, 0, 20.0f + i * 10.0f), Quaternion.identity).gameObject;
 
-			p.solTans = gameObject.transform;
+            Movement1 p = go.GetComponent<Movement1>();
 
-			p.velocidad = Random.Range(2, 40);
+            go.GetComponent<MeshRenderer>().material = planetMaterials[Random.Range(0, planetMaterials.Count)];
 
-			p.rotationVector = new Vector3 ((int)Random.Range(0.01f,1.99f), (int)Random.Range(0.01f, 1.99f), (int)Random.Range(0.01f, 1.99f));
+            p.radius = Random.Range(2, 6);
 
-			p.rotationSpeed = Random.Range(5, 10);
+            p.solTans = gameObject.transform;
 
-			generatedPlanets.Add(p);
-			cam.planets.Add(go);
-		}
-	}
+            p.velocidad = Random.Range(2, 40);
 
-	void Update()
-	{
-		time += Time.deltaTime; //eliminar?
-	}
+            p.rotationVector = new Vector3((int)Random.Range(0.01f, 1.99f), (int)Random.Range(0.01f, 1.99f), (int)Random.Range(0.01f, 1.99f));
+
+            p.rotationSpeed = Random.Range(5, 10);
+
+            if (i == randAux)
+            {
+                atmosphere.SetActive(true);
+                atmosphere.transform.position = go.transform.position;
+                atmosphere.transform.localScale = go.transform.localScale * 1.5f;
+                atmosphere.transform.parent = go.transform;
+            }
+
+            generatedPlanets.Add(p);
+            cam.planets.Add(go);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 8.0f)
+        {
+            MetheorRain();
+            timer = 0.0f;
+        }
+
+    }
+    void MetheorRain()
+    {
+        for (int i = 0; i < metheorAmount; i++)
+        {
+            Instantiate(metheor, new Vector3(Random.Range(-50, 50), 80.0f, Random.Range(-50, 50)), Quaternion.Euler(Vector3.zero));
+        }
+    }
 }
